be5509cc0873e5c5b7666e2cdaee7d59758deecd
honee8583 <honee8583@naver.com>
2024-03-11 10:43:04 +0900

    [FEAT] 회원가입 로직 작성
    - Account, Transaction, User 레포지토리 생성.
    - 커스텀 예외 클래스 생성.
    - User 서비스 클래스 생성.
    - 동일한 username이 존재하지 않을 경우 회원가입 로직 작성 및 존재할경우 예외 발생.
    - 회원가입 요청, 응답 Dto 클래스 서비스클래스안에 내부 클래스로 생성.

---
+++ src/main/java/shop/mtcoding/bank/service/UserService.java
@@ -0,0 +1,68 @@
+package shop.mtcoding.bank.service;
+
+import java.util.Optional;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import shop.mtcoding.bank.domain.user.User;
+import shop.mtcoding.bank.domain.user.UserEnum;
+import shop.mtcoding.bank.domain.user.UserRepository;
+import shop.mtcoding.bank.handler.ex.CustomApiException;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Transactional
+    public JoinRespDto 회원가입(JoinReqDto joinReqDto) {
+        Optional<User> userOp = userRepository.findByUsername(joinReqDto.getUsername());
+        if (userOp.isPresent()) {
+            throw new CustomApiException("동일한 username이 존재합니다");
+        }
+
+        User userPS = userRepository.save(joinReqDto.toEntity(passwordEncoder));
+
+        return new JoinRespDto(userPS);
+    }
+
+    @Getter
+    @Setter
+    public static class JoinRespDto {
+        private Long id;
+        private String username;
+        private String fullname;
+
+        public JoinRespDto(User user) {
+            this.id = user.getId();
+            this.username = user.getUsername();
+            this.fullname = user.getFullname();
+        }
+    }
+
+    @Getter
+    @Setter
+    public static class JoinReqDto {
+        private String username;
+        private String password;
+        private String email;
+        private String fullname;
+
+        public User toEntity(BCryptPasswordEncoder passwordEncoder) {
+            return User.builder()
+                    .username(username)
+                    .password(passwordEncoder.encode(password))
+                    .email(email)
+                    .fullname(fullname)
+                    .role(UserEnum.CUSTOMER)
+                    .build();
+        }
+    }
+}
