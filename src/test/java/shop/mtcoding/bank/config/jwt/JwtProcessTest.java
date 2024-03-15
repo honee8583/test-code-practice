139c98583869bacfdcff158936c485169ed25441
honee8583 <honee8583@naver.com>
2024-03-15 17:25:30 +0900

    [TEST] JWTProcess 테스트 코드 리팩토링

--- src/test/java/shop/mtcoding/bank/config/jwt/JwtProcessTest.java
+++ src/test/java/shop/mtcoding/bank/config/jwt/JwtProcessTest.java
@@ -10,14 +10,17 @@ import shop.mtcoding.bank.domain.user.UserEnum;
 
 class JwtProcessTest {
 
+    private String createToken() {
+        User user = User.builder().id(1L).role(UserEnum.CUSTOMER).build();
+        LoginUser loginUser = new LoginUser(user);
+        return JwtProcess.create(loginUser);
+    }
+
     @Test
     void create_test() {
         // given
-        User user = User.builder().id(1L).role(UserEnum.CUSTOMER).build();  // JWT를 생성할 때 필요한 정보로만 구성
-        LoginUser loginUser = new LoginUser(user);
-
         // when
-        String jwtToken = JwtProcess.create(loginUser);
+        String jwtToken = createToken();
         System.out.println("테스트: " + jwtToken);
 
         // then
@@ -27,14 +30,12 @@ class JwtProcessTest {
     @Test
     void verify_test() {
         // given
-        User user = User.builder().id(1L).role(UserEnum.CUSTOMER).build();  // JWT를 생성할 때 필요한 정보로만 구성
-        LoginUser loginUser = new LoginUser(user);
-        String jwtToken = JwtProcess.create(loginUser).replace(JwtVO.TOKEN_PREFIX, "");
+        String jwtToken = createToken().replace(JwtVO.TOKEN_PREFIX, "");
         System.out.println("테스트: " + jwtToken);
 
         // when
         LoginUser verifiedLoginUser = JwtProcess.verify(jwtToken);
-        System.out.println("테스트: " + loginUser.getUser().getId());
+        System.out.println("테스트: " + verifiedLoginUser.getUser().getId());
 
         // then
         assertThat(verifiedLoginUser.getUser().getId()).isEqualTo(1L);
